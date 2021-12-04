b0083575b03621b63b11803d51a6351b762472b8
OverEarth <56574543+OverEarth12@users.noreply.github.com>
2021-12-04 21:01:34 +0700

    first commit

---
+++ src/main/java/com/example/tictactoc/TictactocApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictactoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactocApplication.class, args);
+    }
+
+}
