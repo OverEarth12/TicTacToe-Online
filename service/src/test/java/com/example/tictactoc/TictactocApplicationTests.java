fb35f4203e675a06a70a5a4bc4e0617de46b92e0
ApichaJA <junyatanakorn@gmail.com>
2021-12-04 21:13:05 +0700

    [FIX] Folder

---
+++ service/src/test/java/com/example/tictactoc/TictactocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tictactoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
